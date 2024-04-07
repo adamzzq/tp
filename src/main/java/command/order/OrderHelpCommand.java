15d40b3925c07e4a174d75c2bb9460585b275d92
webster <webster.tan01@gmail.com>
2024-04-08 03:45:27 +0800

    OrderHelpCommand.java: Fix incorrect command description in help message

--- src/main/java/command/order/OrderHelpCommand.java
+++ src/main/java/command/order/OrderHelpCommand.java
@@ -4,8 +4,10 @@ public class OrderHelpCommand implements OrderCommand {
     public static void execute() {
         System.out.println("Here are the list of available commands:");
         System.out.println("\thelp: Shows all the commands that can be used.");
-        System.out.println("\tadd -item <item_id> -quantity <quantity_of_item> : Shows the menu of the restaurant.");
-        System.out.println("\tdelete -item <item_id> -quantity <quantity_of_item> : Shows the menu of the restaurant.");
+        System.out.println("\tadd -item <item_id> -quantity <quantity_of_item>: "
+                + "Adds the specified quantity \n\t\tof a particular menu item into the order.");
+        System.out.println("\tdelete -item <item_id> -quantity <quantity_of_item>: "
+                + "Deletes the specified quantity \n\t\tof a particular menu item from the order.");
         System.out.println("\tview menu: Shows the menu of the current order.");
         System.out.println("\tview item: Shows the items in the current order.");
         System.out.println("\tcomplete: Completes the order and returns to the main menu.");
